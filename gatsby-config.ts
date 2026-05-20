e20e11c09f79ba03f492996c30f257c630d33aa4
canadianator <zorozco@ztechaerial.com>
2026-05-20 13:17:30 -0500

    Update gatsby-config.ts

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,7 +4,7 @@ const config: GatsbyConfig = {
   pathPrefix: '/sea-lions-swim-board',
   siteMetadata: {
     title: `Sea Lions Swim Meet Board`,
-    siteUrl: `https://sammons.github.io/sea-lions-swim-board`
+    siteUrl: `https://canadianator.github.io/sea-lions-swim-board`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
